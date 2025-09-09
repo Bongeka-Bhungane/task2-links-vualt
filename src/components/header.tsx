219bd98e30ef757b34eb7c4fadad569e58d08987
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-09-09 14:06:25 +0200

    added a text component with a heading and paragraph, also added a header component and changed the background to grey

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Text from './text'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <Text variant="h2" className='logo'>Links Vault</Text>
+    </div>
+  );
+}
