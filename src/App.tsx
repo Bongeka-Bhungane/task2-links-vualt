f01535fc0b31ac925f517c6d9debe81ee98fd878
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-09-16 09:53:25 +0200

    added a loginSign-up component to attempt making a login page, the layout is done and the pages can shift from login to signup, i have to make the buttons save to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 
 import './App.css'
 import Header from './components/header';
-import Viewer from './components/viewer';
+// import Viewer from './components/viewer';
 import Footer from './components/footer';
+import LoginSignup from './components/loginSignup';
 
 function App() {
 
   return (
     <>
       <Header />
-      <Viewer />
+      {/* <Viewer /> */}
+      <LoginSignup />
       <Footer />
     </>
   );
